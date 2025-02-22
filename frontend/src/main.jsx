487e09ac9260fa92da052e54cfe87aa81e3e10b6
Pyro137 <126709211+Pyro137@users.noreply.github.com>
2025-02-22 19:44:59 +0300

    trying to fix deploy issues#12

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import GridBackground from "./components/ui/GridBackground.jsx"
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql",  
   cache: new InMemoryCache(),
   credentials:"include"
-});
+}); 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
